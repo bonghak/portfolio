6a39094b2fea8ba0e1d3b954465c8fad14057436
bonghak <85983093+bonghak@users.noreply.github.com>
2026-09-12 19:40:31 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-
-  const base =
-    mode === 'production'
-      ? env.VITE_BASE_PATH || '/portfolio/'
-      : '/'
-
-  return {
-    base,
-    plugins: [react()],
-  }
+export default defineConfig({
+  base: '/portfolio/',
+  plugins: [react()],
 })
